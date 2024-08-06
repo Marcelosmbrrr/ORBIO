7611aa0df71c8c06d554d8215b5c0e42efa86102
marcelosmbr <development.smbr@gmail.com>
2024-08-05 22:50:01 -0300

    default managers

--- database/seeders/ManagerSeeder.php
+++ database/seeders/ManagerSeeder.php
@@ -13,17 +13,39 @@ class ManagerSeeder extends Seeder
      */
     public function run(): void
     {
-        $manager = User::create([
-            'public_id' => Str::uuid(),
-            'name' => 'Gerente',
-            'role' => 'gerente',
-            'email' => 'gerente1@gmail.com',
-            'password' => '12345',
-            'email_verified_at' => now(),
-        ]);
+        $managers = [
+            [
+                'public_id' => Str::uuid(),
+                'name' => 'Bruno Siqueira',
+                'role' => 'gerente',
+                'email' => 'bruno.siqueira@iffarroupilha.edu.br',
+                'password' => '12345',
+                'email_verified_at' => now(),
+            ],
+            [
+                'public_id' => Str::uuid(),
+                'name' => 'Nicholas Matias',
+                'role' => 'gerente',
+                'email' => 'nickmatias@gmail.com',
+                'password' => '12345',
+                'email_verified_at' => now(),
+            ],
+            [
+                'public_id' => Str::uuid(),
+                'name' => 'Marcelo Moreira',
+                'role' => 'gerente',
+                'email' => 'marcelosmbr2015@gmail.com',
+                'password' => '12345',
+                'email_verified_at' => now(),
+            ]
+        ];
 
-        $manager->address()->create();
-        $manager->document()->create();
-        $manager->contact()->create();
+        foreach ($managers as $manager_data) {
+            $manager = User::create($manager_data);
+            $manager->address()->create();
+            $manager->document()->create();
+            $manager->contact()->create();
+        }
     }
+
 }
